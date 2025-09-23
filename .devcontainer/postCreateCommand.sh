42d9735c8f138d54d221ca4c34a8950c69baa9d7
Jorge <xxopxe@gmail.com>
2025-09-23 13:55:52 +0000

    Initial port to pico-rosso based oruga. wip.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
 cd /oruga_ws
-sudo rosdep install --from-paths src --ignore-src -y
+sudo -E rosdep install --from-paths src --ignore-src -y
+
+# provide a default version (useful when running in a login shell)
+if [[ -z "${GZ_VERSION}" ]]; then
+  export GZ_VERSION=harmonic
+fi
+
 colcon build --symlink-install --cmake-args -DBUILD_TESTING=ON
+source /oruga_ws/install/setup.sh
+
